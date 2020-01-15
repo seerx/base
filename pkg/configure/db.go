936e9733735c55cd458aee6c4a4630afe0415331
dotjava <wisenow@163.com>
2020-01-15 17:19:14 +0800

    设置 json tag
    service 配置增加生成 http.Server 的能力

--- pkg/configure/db.go
+++ pkg/configure/db.go
@@ -3,14 +3,14 @@ package configure
 import "fmt"
 
 type DBConf struct {
-	Type           string `yaml:"type"`           // 数据库: pgsql,mysql ...
-	Host           string `yaml:"host"`           // 数据库地址
-	Port           int    `yaml:"port"`           // 端口
-	User           string `yaml:"user"`           // 用户
-	Passwd         string `yaml:"passwd"`         // 密码
-	DB             string `yaml:"db"`             // 数据库名称
-	SSLMode        string `yaml:"sslMode"`        // pgsql 专用，默认为 disable
-	ConnectTimeout int    `yaml:"connectTimeout"` // 连接超时设置，默认为 10
+	Type           string `json:"type" yaml:"type"`                     // 数据库: pgsql,mysql ...
+	Host           string `json:"host" yaml:"host"`                     // 数据库地址
+	Port           int    `json:"port" yaml:"port"`                     // 端口
+	User           string `json:"user" yaml:"user"`                     // 用户
+	Passwd         string `json:"passwd" yaml:"passwd"`                 // 密码
+	DB             string `json:"db" yaml:"db"`                         // 数据库名称
+	SSLMode        string `json:"sslMode" yaml:"sslMode"`               // pgsql 专用，默认为 disable
+	ConnectTimeout int    `json:"connectTimeout" yaml:"connectTimeout"` // 连接超时设置，默认为 10
 }
 
 // String 生成数据库连接串
--- pkg/configure/service.go
+++ pkg/configure/service.go
@@ -2,17 +2,20 @@ package configure
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
+
+	"github.com/sirupsen/logrus"
 )
 
 // ServiceConf 对外服务配置信息
 type ServiceConf struct {
-	Addr         string `yaml:"addr"`
-	Port         int    `yaml:"port"`
-	WebURL       string `yaml:"webUrl"`
-	WebFilesPath string `yaml:"webFilesPath"`
-	APIDoc       bool   `yaml:"apiDoc"`
-	APIURL       string `yaml:"apiUrl"`
+	Addr         string `json:"addr" yaml:"addr"`
+	Port         int    `json:"port" yaml:"port"`
+	WebURL       string `json:"webUrl" yaml:"webUrl"`
+	WebFilesPath string `json:"webFilesPath" yaml:"webFilesPath"`
+	APIDoc       bool   `json:"apiDoc" yaml:"apiDoc"`
+	APIURL       string `json:"apiUrl" yaml:"apiUrl"`
 }
 
 func checkPath(path string) string {
@@ -35,4 +38,22 @@ func (s *ServiceConf) GetApiURL() string {
 // HttpAddr 生成 http 监听的地址
 func (s *ServiceConf) HttpAddr() string {
 	return fmt.Sprintf("%s:%d", s.Addr, s.Port)
+}
+
+// CreateHttpServer 根据配置信息创建 http Server
+func (s *ServiceConf) CreateHttpServer(apiHandler http.Handler, log *logrus.Logger) (*http.Server, *http.ServeMux) {
+	mux := &http.ServeMux{}
+	log.Infof("Server is bind to [%s]", s.HttpAddr())
+	svr := &http.Server{
+		Addr:    s.HttpAddr(),
+		Handler: mux,
+	}
+	log.Infof("Api url bind to [%s]", s.GetApiURL())
+	mux.Handle(s.GetApiURL(), apiHandler)
+	if s.WebFilesPath != "" {
+		log.Infof("Static page bind to [%s]", s.GetWebURL())
+		mux.Handle(s.GetWebURL(), http.FileServer(http.Dir(s.WebFilesPath)))
+	}
+
+	return svr, mux
 }
