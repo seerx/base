36fac62d9be3dfa197ea4839318aeccf354ac487
dotjava <wisenow@163.com>
2020-01-16 09:08:16 +0800

    数据库配置的 Type 转为 Dialect

--- pkg/configure/db.go
+++ pkg/configure/db.go
@@ -3,11 +3,11 @@ package configure
 import "fmt"
 
 type DBConf struct {
-	Type           string `json:"type" yaml:"type"`                     // 数据库: pgsql,mysql ...
+	Dialect        string `json:"dialect" yaml:"type"`                  // 数据库: postgres,mysql ...
 	Host           string `json:"host" yaml:"host"`                     // 数据库地址
 	Port           int    `json:"port" yaml:"port"`                     // 端口
 	User           string `json:"user" yaml:"user"`                     // 用户
-	Passwd         string `json:"passwd" yaml:"passwd"`                 // 密码
+	Password       string `json:"passwd" yaml:"passwd"`                 // 密码
 	DB             string `json:"db" yaml:"db"`                         // 数据库名称
 	SSLMode        string `json:"sslMode" yaml:"sslMode"`               // pgsql 专用，默认为 disable
 	ConnectTimeout int    `json:"connectTimeout" yaml:"connectTimeout"` // 连接超时设置，默认为 10
@@ -15,10 +15,11 @@ type DBConf struct {
 
 // String 生成数据库连接串
 func (dbc *DBConf) String() string {
-	if dbc.Type == "pgsql" || dbc.Type == "postgresql" {
+	if dbc.Dialect == "pgsql" || dbc.Dialect == "postgres" || dbc.Dialect == "postgresql" {
+		dbc.Dialect = "postgres"
 		return pgsql(dbc)
 	}
-	panic(fmt.Errorf("Do not support database [%s]", dbc.Type))
+	panic(fmt.Errorf("Do not support database [%s]", dbc.Dialect))
 }
 
 func pgsql(dbc *DBConf) string {
@@ -29,5 +30,5 @@ func pgsql(dbc *DBConf) string {
 	if dbc.ConnectTimeout == 0 {
 		dbc.ConnectTimeout = 10
 	}
-	return fmt.Sprintf(pgTemplate, dbc.Host, dbc.Port, dbc.User, dbc.DB, dbc.Passwd, dbc.SSLMode, dbc.ConnectTimeout)
+	return fmt.Sprintf(pgTemplate, dbc.Host, dbc.Port, dbc.User, dbc.DB, dbc.Password, dbc.SSLMode, dbc.ConnectTimeout)
 }
