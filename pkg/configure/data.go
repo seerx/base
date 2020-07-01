2a073ac550f8377db86f5044d0cc27c2aeed10ea
dotjava <wisenow@163.com>
2020-07-01 15:35:21 +0800

    修改配置获取方式

--- pkg/configure/data.go
+++ pkg/configure/data.go
@@ -4,12 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 )
 
 // GetConfigureData 获取运行环境信息，即配置文件
-func GetConfigureData(configureFileArgName string, configureEnvArgName string) []byte {
+func GetConfigureData(configureFileArgName string, configureEnvArgName string) ([]byte, error) {
 	var file string
 	var env string
 	msgFile := fmt.Sprintf("Please input configure file path with -%s", configureFileArgName)
@@ -17,24 +16,39 @@ func GetConfigureData(configureFileArgName string, configureEnvArgName string) [
 	flag.StringVar(&file, configureFileArgName, "", msgFile)
 	flag.StringVar(&env, configureEnvArgName, "", msgEnv)
 	flag.Parse()
+	var errOfFile error
 	if file != "" {
-		// 文件优先
+		// 配置文件优先
 		fd, err := os.Open(file)
 		if err != nil {
-			log.Fatal(fmt.Errorf("Open configure file error: [%w]", err))
+			errOfFile = fmt.Errorf("Open configure file error: [%w]", err)
+			goto second
+			// return nil, fmt.Errorf("Open configure file error: [%w]", err)
 		}
 		defer fd.Close()
 		val, err := ioutil.ReadAll(fd)
 		if err != nil {
-			log.Fatal(fmt.Errorf("Read configure file error: [%w]", err))
+			errOfFile = fmt.Errorf("Read configure file error: [%w]", err)
+			goto second
+			// return nil, fmt.Errorf("Read configure file error: [%w]", err)
 		}
-
-		return val
+		// 返回文件内容
+		return val, nil
 	}
+second:
 	if env != "" {
-		// 使用环境变量
-		return []byte(env)
+		// 第二选择,使用环境变量，此时可能同时返回错误信息，该错误只需要记录即可
+		// 第一个参数不是 nil
+		cfgEnv := os.Getenv(env)
+		if cfgEnv == "" {
+			// 环境变量中没有对应的变量
+			return nil, fmt.Errorf("No environment item set by name [%s]", env)
+		}
+		return []byte(cfgEnv), errOfFile
 	}
-	log.Fatal(fmt.Errorf("Please input configurations from file by -%s or from environment by -%s", configureFileArgName, configureEnvArgName))
-	return nil
+	if errOfFile != nil {
+		// 没有环境变量，但是读取配置文件时发生错误，返回错误信息
+		return nil, errOfFile
+	}
+	return nil, fmt.Errorf("Please input configurations from file by -%s or from environment by -%s", configureFileArgName, configureEnvArgName)
 }
